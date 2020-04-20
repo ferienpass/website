77d2808ccb391c64bacf808407bfd9ddf4fbf449
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2020-04-20 15:11:36 +0200

    Remove copyright footer section

--- components/LayoutFooter.vue
+++ components/LayoutFooter.vue
@@ -24,11 +24,6 @@
           <icon-github class="w-6 h-6"></icon-github>
         </a>
       </div>
-      <div class="mt-8">
-        <p class="text-center text-base leading-6 text-gray-400">
-          &copy; {{ currentDate.getFullYear() }} Richard Henkenjohann
-        </p>
-      </div>
     </div>
   </div>
 </template>
@@ -43,7 +38,6 @@
     components: {IconGithub, IconEmail, LinkEmail},
     data() {
       return {
-        currentDate: new Date,
         navigation: [
           {
             href: '/',
@@ -65,4 +59,3 @@
     }
   }
 </script>
-
